0c8347d4a2670f90f1434d161cc008fb06fdc382
loren <lorenbhanu@gmail.com>
2025-11-07 22:19:26 +0530

    added body

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@ use actix_web::{App, HttpRequest, HttpResponse, HttpServer, web};
 use std::collections::HashMap;
 use std::sync::Arc;
 mod controllers;
+use actix_web::web::Bytes;
 pub use askama;
 pub use askama::Template;
 pub use controllers::*;
+
 // pub use serde::Serialize;
 pub type ArcRenderModel = Arc<dyn RenderModel>;
 
@@ -14,7 +16,7 @@ pub struct RequestContext {
     pub params: HashMap<String, String>,
     pub headers: HeaderMap,
     pub path: String,
-    // you can add more fields like cookies, method, body, etc.
+    pub body: Vec<u8>,
 }
 
 #[derive(Clone)]
@@ -92,65 +94,70 @@ impl Server {
         HttpServer::new(move || {
             App::new()
                 .app_data(shared_routes.clone())
-                .default_service(web::to(|req: HttpRequest, srv: web::Data<Server>| {
-                    let mut params = HashMap::new();
-                    for (key, value) in
-                        req.query_string()
+                .default_service(web::to(
+                    |req: HttpRequest, body: Bytes, srv: web::Data<Server>| {
+                        let mut params = HashMap::new();
+                        for (key, value) in req
+                            .query_string()
                             .split('&')
                             .filter(|s| !s.is_empty())
                             .map(|pair| {
                                 let mut kv = pair.splitn(2, '=');
                                 (kv.next().unwrap_or(""), kv.next().unwrap_or(""))
                             })
-                    {
-                        params.insert(key.to_string(), value.to_string());
-                    }
-
-                    let ctx = RequestContext {
-                        path: req.path().to_string(),
-                        headers: req.headers().clone(),
-                        params,
-                    };
-
-                    let result = srv.handle_request(ctx);
-
-                    let body = match result {
-                        ActionResult::Html(s) => {
-                            HttpResponse::Ok().content_type("text/html").body(s)
+                        {
+                            params.insert(key.to_string(), value.to_string());
                         }
-                        ActionResult::View(renderer_arc) => match renderer_arc.render_html() {
-                            Ok(html) => HttpResponse::Ok().content_type("text/html").body(html),
-                            Err(e) => {
-                                eprintln!("Askama Rendering Error: {}", e);
-                                HttpResponse::InternalServerError()
-                                    .body(format!("<h1>Template Rendering Error: {}</h1>", e))
-                            }
-                        },
 
-                        ActionResult::Redirect(url) => HttpResponse::Found()
-                            .append_header(("Location", url))
-                            .finish(),
-                        ActionResult::File(path) => {
-                            let path = format!("wwwroot/{}", path);
-                            match std::fs::read(&path) {
-                                Ok(bytes) => {
-                                    let content_type =
-                                        mime_guess::from_path(&path).first_or_octet_stream();
-                                    HttpResponse::Ok()
-                                        .content_type(content_type.as_ref())
-                                        .body(bytes)
+                        let ctx = RequestContext {
+                            path: req.path().to_string(),
+                            headers: req.headers().clone(),
+                            params,
+                            body: body.to_vec(),
+                        };
+
+                        let result = srv.handle_request(ctx);
+
+                        let body = match result {
+                            ActionResult::Html(s) => {
+                                HttpResponse::Ok().content_type("text/html").body(s)
+                            }
+                            ActionResult::View(renderer_arc) => match renderer_arc.render_html() {
+                                Ok(html) => HttpResponse::Ok().content_type("text/html").body(html),
+                                Err(e) => {
+                                    eprintln!("Askama Rendering Error: {}", e);
+                                    HttpResponse::InternalServerError()
+                                        .body(format!("<h1>Template Rendering Error: {}</h1>", e))
                                 }
-                                Err(_) => HttpResponse::NotFound().body("<h1>404 Not Found</h1>"),
+                            },
+
+                            ActionResult::Redirect(url) => HttpResponse::Found()
+                                .append_header(("Location", url))
+                                .finish(),
+                            ActionResult::File(path) => {
+                                let path = format!("wwwroot/{}", path);
+                                match std::fs::read(&path) {
+                                    Ok(bytes) => {
+                                        let content_type =
+                                            mime_guess::from_path(&path).first_or_octet_stream();
+                                        HttpResponse::Ok()
+                                            .content_type(content_type.as_ref())
+                                            .body(bytes)
+                                    }
+                                    Err(_) => {
+                                        HttpResponse::NotFound().body("<h1>404 Not Found</h1>")
+                                    }
+                                }
                             }
-                        }
 
-                        ActionResult::NotFound => {
-                            HttpResponse::NotFound().body("<h1>404 Not Found</h1>")
-                        }
-                    };
+                            ActionResult::NotFound => {
+                                HttpResponse::NotFound().body("<h1>404 Not Found</h1>")
+                            }
+                        };
 
-                    async move { body }
-                }))
+                        async move { body }
+                    },
+                ))
         })
         .bind(addr)?
         .run()
